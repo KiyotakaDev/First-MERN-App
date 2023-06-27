ba7508667de3baa36e80802f85d650e4a556ba65
きよたか <c4mo.sw@gmail.com>
2023-06-27 12:12:57 -0500

    feat(schema validator): A model Schema and the validator Schema were created

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,10 +6,12 @@ import {
   logout,
 } from "../controllers/auth.controller.js";
 import { authRequired } from "../middlewares/authValidator.js";
+import { validateSchema } from '../middlewares/schemaValidator.js'
+import { registerShema } from '../schemas/auth.schema.js'
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", validateSchema(registerShema), register);
 router.post("/login", login);
 router.post("/logout", logout);
 router.get("/profile", authRequired, profile);
