4ad3a5ebe4cce576e814a9ef95d09057b78f5b86
きよたか <c4mo.sw@gmail.com>
2023-06-27 12:13:26 -0500

    feat(schema validator): A model Schema and the validator Schema were created

---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const registerShema = z.object({
+  username: z.string({
+    required_error: "Username is required",
+  }),
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "Invalid email",
+    }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(6, {
+      message: "Password must be at least 6 characters",
+    }),
+});
