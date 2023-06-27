f781908ca066fa2578a69533a34f246794298656
きよたか <c4mo.sw@gmail.com>
2023-06-27 11:25:17 -0500

    feat(logout): Introducing logout functionality

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { register, login, profile } from "../controllers/auth.controller.js";
-import { authRequired } from '../middlewares/authValidator.js'
+import {
+  register,
+  login,
+  profile,
+  logout,
+} from "../controllers/auth.controller.js";
+import { authRequired } from "../middlewares/authValidator.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 router.get("/profile", authRequired, profile);
 
 export default router;
