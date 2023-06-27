3687703df74d0b4f6678d1d99ef7b5e7cfe10ff6
きよたか <c4mo.sw@gmail.com>
2023-06-27 12:18:41 -0500

    feat(login schema): login schema functionality was created

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,12 +7,12 @@ import {
 } from "../controllers/auth.controller.js";
 import { authRequired } from "../middlewares/authValidator.js";
 import { validateSchema } from '../middlewares/schemaValidator.js'
-import { registerShema } from '../schemas/auth.schema.js'
+import { registerShema, loginSchema } from '../schemas/auth.schema.js'
 
 const router = Router();
 
 router.post("/register", validateSchema(registerShema), register);
-router.post("/login", login);
+router.post("/login", validateSchema(loginSchema), login);
 router.post("/logout", logout);
 router.get("/profile", authRequired, profile);
 
