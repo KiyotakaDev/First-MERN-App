20966d21163e7493a886320c8cd4cb92587d5ca2
きよたか <c4mo.sw@gmail.com>
2023-06-28 07:30:35 -0500

    feat(schemas): Creating a task schema for createTask validation

--- src/middlewares/schemaValidator.js
+++ src/middlewares/schemaValidator.js
@@ -4,7 +4,7 @@ export const validateSchema = (schema) => (req, res, next) => {
     next();
   } catch (error) {
     return res
-      .status(500)
+      .status(400)
       .json({ error: error.errors.map((error) => error.message) });
   }
 };
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -8,12 +8,19 @@ import {
 } from "../controllers/task.controller.js";
 
 import { authRequired } from "../middlewares/authValidator.js";
+import { validateSchema } from "../middlewares/schemaValidator.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
 
 const router = Router();
 
 router.get("/tasks", authRequired, getAllTasks);
 router.get("/tasks/:id", authRequired, getTask);
-router.post("/tasks", authRequired, createTask);
+router.post(
+  "/tasks",
+  authRequired,
+  validateSchema(createTaskSchema),
+  createTask
+);
 router.put("/tasks/:id", authRequired, updateTask);
 router.delete("/tasks/:id", authRequired, deleteTask);
 
